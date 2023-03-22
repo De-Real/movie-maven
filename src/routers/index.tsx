6b0d8234f0dd6541fd0db01b9504d893df1fcc58
Real <vinarchuk.a.v@.gmail.com>
2023-03-22 12:37:28 +0200

    first deployment arrangement

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -5,29 +5,32 @@ import Preferences from "../pages/Preferences";
 import Profile from "../pages/Profile";
 import { moviesRouter } from "./movies-router";
 
-export const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <RootLayout />,
-		children: [
-			{
-				index: true,
-				element: <Navigate to="movies" />,
-			},
-			{
-				path: "profile",
-				element: <Profile />,
-			},
-			{
-				path: "auth",
-				element: <Auth />,
-			},
-			{
-				path: "preferences",
-				element: <Preferences />,
-			},
-			//Router for particular movie type
-			moviesRouter,
-		],
-	},
-]);
+export const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <RootLayout />,
+			children: [
+				{
+					index: true,
+					element: <Navigate to="movies" />,
+				},
+				{
+					path: "profile",
+					element: <Profile />,
+				},
+				{
+					path: "auth",
+					element: <Auth />,
+				},
+				{
+					path: "preferences",
+					element: <Preferences />,
+				},
+				//Router for particular movie type
+				moviesRouter,
+			],
+		},
+	],
+	{ basename: "/movie-maven" }
+);
